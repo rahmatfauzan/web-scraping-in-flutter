ca7f83505652d50b8249e2dc1433d1e3ebd2d8f9
rahmatfauzan <rahmatfauzan148@gmail.com>
2023-11-15 23:07:38 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.green[100],
+        primaryColor: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Strings to store the extracted Article titles
+  String result1 = 'Result 1';
+  String result2 = 'Result 2';
+  String result3 = 'Result 3';
+
+  // boolean to show CircularProgressIndication
+  // while Web Scraping awaits
+  bool isLoading = false;
+
+  Future<List<String>> extractData() async {
+    // Getting the response from the targeted url
+    final response = await http.Client()
+        .get(Uri.parse('https://detiklife.com/cerita-lucu-humor-terbaru/'));
+
+    // Status Code 200 means response has been received successfully
+    if (response.statusCode == 200) {
+      // Getting the html document from the response
+      var document = parser.parse(response.body);
+      try {
+        // Scraping the first article title
+        var responseString1 =
+            document.getElementsByClassName('entry-content')[0].children[3];
+
+        print(responseString1.text.trim());
+
+        // Scraping the second article title
+        var responseString2 =
+            document.getElementsByClassName('entry-content')[0].children[12];
+
+        print(responseString2.text.trim());
+
+        // Scraping the third article title
+        var responseString3 =
+            document.getElementsByClassName('entry-content')[0].children[17];
+
+        print(responseString3.text.trim());
+
+        // Converting the extracted titles into
+        // string and returning a list of Strings
+        return [
+          responseString1.text.trim(),
+          responseString2.text.trim(),
+          responseString3.text.trim()
+        ];
+      } catch (e) {
+        return ['', '', 'ERROR!'];
+      }
+    } else {
+      return ['', '', 'ERROR: ${response.statusCode}.'];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('GeeksForGeeks')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // if isLoading is true show loader
+            // else show Column of Texts
+            isLoading
+                ? CircularProgressIndicator()
+                : Column(
+                    children: [
+                      Text(result1,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Text(result2,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Text(result3,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+            MaterialButton(
+              onPressed: () async {
+                // Setting isLoading true to show the loader
+                setState(() {
+                  isLoading = true;
+                });
+
+                // Awaiting for web scraping function
+                // to return list of strings
+                final response = await extractData();
+
+                // Setting the received strings to be
+                // displayed and making isLoading false
+                // to hide the loader
+                setState(() {
+                  result1 = response[0];
+                  result2 = response[1];
+                  result3 = response[2];
+                  isLoading = false;
+                });
+              },
+              child: Text(
+                'Scrap Data',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.green,
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
